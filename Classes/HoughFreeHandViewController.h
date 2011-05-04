fefac0299efbab0736563bbc6a5f5de55d15caea
Joakim Fernstad <pocke@Shaitan.local>
2011-05-03 21:57:52 -0700

    First test with UITabBarController. Also changed copyright text.

---
+++ Classes/HoughFreeHandViewController.h
@@ -0,0 +1,16 @@
+//
+//  HoughFreeHandViewController.h
+//  HoughDemo
+//
+//  Created by Joakim Fernstad on 5/3/11.
+//  Copyright 2011 Joakim Fernstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HoughFreeHandViewController : UIViewController {
+    
+}
+
+@end
