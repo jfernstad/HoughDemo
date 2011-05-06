3f87e93a1fa0b3d9d57ba6ebeebec59a8ebe18b9
Joakim Fernstad <pocke@Shaitan.local>
2011-05-05 22:13:38 -0700

    Adding image view controller

---
+++ Classes/HoughImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  HoughImageViewController.h
+//  HoughDemo
+//
+//  Created by Joakim Fernstad on 5/5/11.
+//  Copyright 2011 Joakim Fernstad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface HoughImageViewController : UIViewController {
+    
+}
+
+@end
